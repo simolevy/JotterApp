966ca7e19bc8b5d52d54135e2a7cafdc9999f1be
Simone Levy <simonelevy@MacBook-Air-83.local>
2024-10-13 18:14:49 -0400

    Front End work done

---
+++ client/jotterapp/src/Post.js
@@ -0,0 +1,20 @@
+export default function Post() {
+    return (
+
+        <div className="post">
+          <div className='image'>
+            <img src='https://i.ytimg.com/vi/QEolkcw1W40/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLDdFfIYelWvMRvlxB4YvvImxM-YpA' alt='' ></img>
+          </div>
+          <div className="texts">
+            <h2>Miku sentenced to death</h2>
+            <p className='info'>
+              <span className='author'>Simo Levy </span>
+              <time>2024-10-06 16:38</time>
+            </p>
+            <p className='summary'>Miku has been sentenced to death in Texas.</p>
+          </div>
+        </div>
+
+
+    );
+}
